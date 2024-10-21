10ad7a3c3bb2ca58f058e5d26c2b044849a98798
Atiqah <natiqah04.23@ichat.sp.edu.sg>
2024-10-21 10:25:10 +0800

    Added median temperature

--- Lab_2.py
+++ Lab_2.py
@@ -11,6 +11,9 @@ def main():
     print("Minimum Temperature: " + str(min_max[0])) #access minimum value, index 0
     print("Maximum Temperature: " + str(min_max[1])) #access maximum value, index 1
 
+    median_temp = calc_median_temp(num_list)
+    print("Median Temperature: " + str(median_temp))
+
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
 
@@ -30,6 +33,16 @@ def find_min_max(temp):
     max_temp = max(temp)
     
     return [min_temp, max_temp] #return as integer list
+
+def calc_median_temp(temp):
+    temp.sort()
+    n = len(temp)
+    if n % 2 == 0:
+        median = (temp[n//2 - 1] + temp[n//2]) / 2
+    else:
+        median = temp[n//2]
+    return median
+    
 
 if __name__=="__main__":
     main()
