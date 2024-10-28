719aa4f34b276e975a5fc912bbd0d31d40b019bb
Atiqah <natiqah04.23@ichat.sp.edu.sg>
2024-10-28 09:00:40 +0800

    Modify to add return values and weight classification

--- BMI.py
+++ BMI.py
@@ -6,11 +6,14 @@ def calculate_bmi(height, weight):
     print("Your BMI is = " + str(BMI))
 
     if BMI < 18.5:
-        print("Weight Classification: Underweight")
+        print("Weight Classification: Under Weight")
+        return -1
     elif 18.5 <= BMI <= 25.0:
         print("Weight Classification: Normal Weight")
+        return 0
     elif BMI > 25.0:
-        print("Weight Classification: Overweight")
+        print("Weight Classification: Over Weight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
 
